5a1c386a4d1a9ec435f0eb4bbc2b495b84f2af9d
redmond22 <127736623+redmond22@users.noreply.github.com>
2025-01-13 23:10:19 +0000

    Add files via upload

---
+++ js/footer.scripts.js
@@ -0,0 +1,14 @@
+var footerHeader = document.getElementById('extrasFooterHeader');
+var footerDescription = document.getElementById('footerHeadDescription');
+var footerAddress = document.getElementById('footerAddress');
+
+
+// footerDescription.addEventListner('load', () => {
+//     footerDescription.style.color = '#fff';
+// })
+
+ window.onload = function () {
+    footerHeader.style.color = '#fff';
+    footerHeadDescription.style.color = '#eee';
+    footerAddress.style.color = 'blue';
+};
